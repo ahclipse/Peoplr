01d21f2afb5724abf03433376cfba50484557c91
Shannon Cox <shannon.cox00@gmail.com>
2015-05-10 07:35:02 -0500

    Moved full Tag class out from MainActivity.java and into its own file

--- app/src/main/java/com/example/ahaag/peoplr/MainActivity.java
+++ app/src/main/java/com/example/ahaag/peoplr/MainActivity.java
@@ -164,30 +164,6 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
     }
 
-//    public class Tag {
-//
-//        @SerializedName("updated_at")
-//        private String updated_at;
-//
-//        @SerializedName("created_at")
-//        private String created_at;
-//
-//        @SerializedName("id")
-//        private String id;
-//
-//        @SerializedName("name")
-//        private String name;
-//
-//        public final Integer getId() {
-//            return Integer.parseInt(this.id);
-//        }
-//
-//        public final String getName() {
-//            return this.name;
-//        }
-//
-//    }
-
     protected void onTagResponse(String response) {
 
         Gson gson = new Gson();
